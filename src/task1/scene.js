7ee006a2c9e96968d7d5a3f973dfeaa34d29d5a4
Birgitte Haavardsholm <birgitte.haavardsholm@gmail.com>
2019-10-12 11:04:25 +0200

    Make task1

--- src/task1/scene.js
+++ src/task1/scene.js
@@ -1,54 +1,78 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
+let camera, scene, renderer, cube;
 
-let camera, scene, renderer;
+const onWindowResize = function() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+};
 
-
-let windowHalfX = window.innerWidth / 2;
-let windowHalfY = window.innerHeight / 2;
-
-
-function initScene() {
-
+const initScene = function(element) {
   //Init camera
-  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 2000 );
-  camera.position.z = 1000;
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    2000
+  );
+  camera.position.z = 5;
 
   //Init scene
   scene = new THREE.Scene();
-  scene.fog = new THREE.FogExp2( 0x000000, 0.0008 );
+  scene.fog = new THREE.FogExp2(0x000000, 0.0008);
 
   //Init rendere
   renderer = new THREE.WebGLRenderer();
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  element.appendChild(renderer.domElement);
 
-  window.addEventListener( 'resize', onWindowResize, false );
+  window.addEventListener("resize", onWindowResize, false);
+};
 
-  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const initParticles = function() {
+  /**
+   * Task1: Init a cube
+
+   https://threejs.org/docs/#api/en/geometries/BoxGeometry
+   **/
+  var geometry = new THREE.BoxGeometry(1, 1, 1);
   var material = new THREE.MeshNormalMaterial();
-  var cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+  cube = new THREE.Mesh(geometry, material);
+  cube.rotateY(0.5);
+  cube.rotateX(0.5);
+  scene.add(cube);
 
-  animate()
-}
+  /**
+   * Task 3: Make multiple cubes.
 
-function onWindowResize() {
-  windowHalfX = window.innerWidth / 2;
-  windowHalfY = window.innerHeight / 2;
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-}
+   Maybe give them an initial position, rotation, material with different colors
+   **/
+};
 
 function animate() {
-	requestAnimationFrame( animate );
-  console.log("heia")
-  renderer.render( scene, camera );
+  requestAnimationFrame(animate);
+  /**
+   * Task2: Make cube rotate
 
+   Use .rotateX, .rotateY, .rotateZ with a radian and see the effects
+   Note: this happens as many times as the frame rate. If the frame rate is 60,
+   then this will happens 60 times in one second
+   **/
+
+  /**
+    * Task 4: Move the cubes
+
+    Try to give the cubes some movements by translating, rotating and scaling
+    **/
+  renderer.render(scene, camera);
 }
 
 export default {
-  initScene : initScene
-}
+  init: function(element) {
+    initScene(element);
+    initParticles();
+    animate();
+  }
+};
